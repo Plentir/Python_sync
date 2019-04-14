a80f9adeb74dc9caec5574dccfb4101a697207fa
Plentir <yjy990810@naver.com>
2019-04-15 03:15:09 +0900

    과제 업데이트

--- 융합전공 튜터링/과제/ExternalFunc.py
+++ 융합전공 튜터링/과제/ExternalFunc.py
@@ -8,3 +8,41 @@ py - 1p,2p,3p 확장자를 .txt로 바꾸세요
 bat - 현재 시간(밀리초까지)
 jpg - a,b,c,d...
 """
+import os
+import time
+
+def MkMvRn(folder, file):
+    os.mkdir("c:\\%s" %folder) # 디렉터리 생성. - 1번
+    os.system("move %s c:\\%s\\%s" %(file, folder, file.split("\\")[-1])) # 생성한 디렉터리로 파일 이동. - 2번
+    os.startfile("c:\\%s\\%s" %(folder, file.split("\\")[-1])) # 이동한 파일을 실행. - 3번
+    return 1
+
+def AutoCat(path):
+    files = os.listdir(path)
+    file_list = []
+    for file in files: # 파일 목록 생성
+        file_list.append(file)
+
+    os.chdir(path)
+    for i in range(len(file_list)):
+        if file_list[i].split(".")[-1] == "txt":
+            for i in range(len(file_list)): # .txt 파일 이름 변경(완성)
+                os.rename(file_list[i], "%s.txt" %i)
+        
+        elif "p" in file_list[i].split(".")[-1][-1]:
+            for i in range(len(file_list)): # .py 파일 이름 변경(완성)
+                os.rename(file_list[i], "%s.txt" %(i, file_list[i].split(".")[0]))
+
+        elif file_list[i].split(".")[-1] == "bat":
+            for i in range(len(file_list)): # .bat 파일 이름 변경(미완)
+                os.rename(file_list[i], "%s.bat" %(time.time_ns()))
+
+        elif file_list[i].split(".")[-1] == "jpg":
+            for i in range(len(file_list)): # .jpg 파일 이름 변경(완성)
+                os.rename(file_list[i], "%s.jpg" %chr(i + 65).lower())
+        
+    return 1
+    
+if __name__ == "__main__":
+    #MkMvRn("윤주영", "c:\\users\\yjy99\\desktop\\과제.zip")
+    AutoCat("C:\\윤주영\\과제\\과제")
