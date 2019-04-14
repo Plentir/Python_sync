580ca22be9ce5b7d1ccc54b8e25334b19f7a5bc3
Plentir <yjy990810@naver.com>
2019-04-14 23:47:58 +0900

    파일 정리

--- 과제/NumBaseball.py
+++ 과제/NumBaseball.py
@@ -18,14 +18,22 @@ def NumBB():
     hist_ans = []
     flag_rtry = 1 # 게임 반복 여부. 1 = 반복, 0 = 종료
     while flag_rtry == 1:
-
         flag_inp = 0
         while flag_inp == 0: # 유효한 값을 받을 때까지 반복
             try:
-                p_ans = str(input("""0부터 9까지의 서로 다른 정수 3개를 입력하세요.
-입력한 숫자: """))
-                if int(p_ans) ==
-            except ValueError: # int("str") = ValueError 오류
+                p_ans = input("""0부터 9까지의 서로 다른 정수 3개를 입력하세요.
+입력한 숫자: """)
+                if int(p_ans) >= 0 and int(p_ans) <= 999 and len(p_ans) == 3: # 음수 및 자리 수 판별, ValueError 트리거.
+                    for i in p_ans:
+                        if p_ans.count(i) != 1 : # 각 자리 숫자 중복 여부 판별
+                            flag_inp = 0
+                            print("중복된 숫자가 있습니다.\n")
+                            break
+                        else:
+                            flag_inp = 1
+                else:
+                    print("0부터 9까지의 수로 이루어진 세 자리 수가 아닙니다.\n")
+            except ValueError: # 숫자가 아닌 자료형 판별
                 print("숫자가 아닌 문자가 포함되어 있습니다.\n")
                 flag_inp = 0
         hist_ans.append(int(p_ans)) # 입력값 저장
@@ -43,19 +51,22 @@ def NumBB():
 3 strikes OUT!
 게임 횟수: %s
 게임 기록: %s\n""" %(strk, ball, len(hist_ans), hist_ans))
+            return 1 # 종료
         else:
             print("""%sS, %sB.
 게임 횟수: %s
 게임 기록: %s\n""" %(strk, ball, len(hist_ans), hist_ans))
 
-        while 1: # 게임 반복 여부 확인
+        while 1: # 게임 반복 여부 확인(틀린 경우.)
             rtry = input("게임을 계속 하시겠습니까? y/n\n")
-            if rtry == "y":
-                flag_rtry = 1
-                break
-            elif rtry == "n":
+            if rtry == "n":
                 flag_rtry = 0
+                return 1 # 종료
+            elif rtry == "y":
+                flag_rtry = 1
                 break
             else:
                 input("y 또는 n으로 입력해 주십시오.\n")
-NumBB()
+
+if __name__ == "__main__":
+    NumBB()
