6100ee79b397f93ea0fd3c5cb948eeaaca7db34a
Plentir <yjy990810@naver.com>
2019-04-16 01:33:52 +0900

    주석 수정

--- 융합전공 튜터링/과제/ExternalFunc.py
+++ 융합전공 튜터링/과제/ExternalFunc.py
@@ -11,7 +11,7 @@ jpg - a,b,c,d...
 import os
 import time
 
-def MkMvRn(folder, file):
+def MkMvRn(folder, file): # 디렉터리 생성 ~ 압축 해제
     try:
         os.mkdir("c:\\%s" %folder) # 디렉터리 생성. - 1번
         os.system("move %s c:\\%s\\%s" %(file, folder, file.split("\\")[-1])) # 생성한 디렉터리로 파일 이동. - 2번
@@ -20,46 +20,46 @@ def MkMvRn(folder, file):
             print("Work in Progress... %s seconds remain." %(6 - i))
             time.sleep(1) # 압축 해제 완료 시까지 대기 = 5초
 
-    except FileExistsError:
+    except FileExistsError: # 파일 또는 폴더가 이미 존재할 때
         print("Can not make directory. The folder or file already exists.")
-    except FileNotFoundError:
+    except FileNotFoundError: # 파일 또는 폴더가 존재하지 않을 때
         print("The file is not found. Can not go on the processes.")
     return 1
 
-def AutoCat(path):
+def AutoCat(path): # 파일 분류 및 이름 재설정
     try:
         files = os.listdir(path)
         file_list = []
         for f in files: # 파일 목록 생성(변경할 파일 목록 작성)
             file_list.append(f)
 
-        os.chdir(path)
+        os.chdir(path) # 압축을 해제한 폴더 내에서 작업 진행.
         cnt_txt = 0
         cnt_py = 0
         cnt_jpg = 0
         for i in file_list: # 변경 과정 반복
-            if i.split(".")[-1] == "txt": # .txt 파일 이름 변경(완성)
+            if i.split(".")[-1] == "txt": # .txt 파일 이름 변경
                 cnt_txt += 1
                 os.rename(i, "%s.txt" %cnt_txt)
             
-            elif i.split(".")[-1] == "py": # .py 파일 이름 변경(완성)
+            elif i.split(".")[-1] == "py": # .py 파일 이름 변경
                 cnt_py += 1
                 os.rename(i, "%sp.txt" %cnt_py)
 
-            elif i.split(".")[-1] == "bat": # .bat 파일 이름 변경(완성)
+            elif i.split(".")[-1] == "bat": # .bat 파일 이름 변경
                 now = time.strftime("%Y%m%d_%Hh_%Mm_%S", time.localtime(time.time()))
                 now += str(time.time())[10:14]
                 os.rename(i, "%ss.bat" %now)
                 time.sleep(0.001) # 파일 이름 충돌 방지
                 
-            elif i.split(".")[-1] == "jpg": # .jpg 파일 이름 변경(완성)
+            elif i.split(".")[-1] == "jpg": # .jpg 파일 이름 변경
                 cnt_jpg += 1
                 os.rename(i, "%s.jpg" %chr(cnt_jpg + 64).lower())
         print("File re-naming is successfully done.")
         
-    except FileExistsError:
+    except FileExistsError: # 파일 또는 폴더가 이미 존재할 때
         print("Can not make directory. The folder or file already exists.")
-    except FileNotFoundError:
+    except FileNotFoundError: # 파일 또는 폴더가 존재하지 않을 때
         print("The file is not found. Can not go on the processes.")
     return 1
 
