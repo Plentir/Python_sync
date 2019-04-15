18acf094edc25f076eddc9e04c90e556c52abbe1
Plentir <yjy990810@naver.com>
2019-04-16 01:45:34 +0900

    주석 추가

--- 기타/Sandbox.py
+++ 기타/Sandbox.py
@@ -1,3 +1,4 @@
-print("hello", ",", "world!", sep = "", end = "" )
+print("hello", ",", "world!", sep = "__" , end = " " )
+# sep: 각 요소 사이에 들어갈 문자(구분자), end: 출력할 내용의 끝에 붙을 내용
 print("HELLO, WORLD!")
 print("Last line")
