c22445b4c7d9cc208b04ab2e0adc602bb0169327
Plentir <yjy990810@naver.com>
2019-04-17 02:06:36 +0900

    찐 최종

--- 융합전공 튜터링/과제/NumBaseball.py
+++ 융합전공 튜터링/과제/NumBaseball.py
@@ -21,20 +21,19 @@ def NumBB():
         flag_inp = 0
         while flag_inp == 0: # 유효한 값을 받을 때까지 반복
             try:
-                p_ans = input("""\n0부터 9까지의 서로 다른 정수 3개를 입력하세요.
-입력한 숫자: """)
+                p_ans = input("\n0부터 9까지의 서로 다른 정수 3개를 입력하세요.\n입력: ")
                 if int(p_ans) >= 0 and int(p_ans) <= 999 and len(p_ans) == 3: # 음수 및 자리 수 판별, ValueError 트리거.
                     for i in p_ans:
                         if p_ans.count(i) != 1 : # 각 자리 숫자 중복 여부 판별
                             flag_inp = 0
-                            print("중복된 숫자가 있습니다.\n")
+                            print("중복된 숫자가 있습니다.")
                             break
                         else:
                             flag_inp = 1
                 else:
-                    print("0부터 9까지의 수로 이루어진 세 자리 수가 아닙니다.\n")
+                    print("0부터 9까지의 수로 이루어진 세 자리 수가 아닙니다.")
             except ValueError: # 숫자가 아닌 자료형 판별
-                print("숫자가 아닌 문자가 포함되어 있습니다.\n")
+                print("숫자가 아닌 문자가 포함되어 있습니다.")
                 flag_inp = 0
 
         strk = 0
@@ -49,17 +48,17 @@ def NumBB():
         if strk == 3: # 게임 결과 출력
             print("""\n%sS, %sB.
 3 strikes OUT!
-게임 횟수: %s
+게임 플레이 횟수: %s회
 게임 기록: %s\n
 게임을 종료합니다.""" %(strk, ball, len(hist_ans), hist_ans))
             return 1 # 종료
         else:
             print("""\n%sS, %sB.
-게임 횟수: %s
+게임 플레이 횟수: %s회
 게임 기록: %s\n""" %(strk, ball, len(hist_ans), hist_ans))
 
         while 1: # 게임 반복 여부 확인(틀린 경우.)
-            rtry = input("게임을 계속 하시겠습니까? (y/n)\n")
+            rtry = input("게임을 계속 하시겠습니까? (y/n)\n입력: ")
             if rtry == "n":
                 flag_rtry = 0
                 print("\n게임을 종료합니다.")
