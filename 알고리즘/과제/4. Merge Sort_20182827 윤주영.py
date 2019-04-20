df729cb538ccd2e478b4a4e00584d1b6cd91e928
Plentir <yjy990810@naver.com>
2019-04-20 19:28:22 +0900

    과제 끝 ㅠ

--- 알고리즘/과제/4. Merge Sort_20182827 윤주영.py
+++ 알고리즘/과제/4. Merge Sort_20182827 윤주영.py
@@ -1,47 +1,30 @@
 # 합병 정렬
-def MrgSort(ary):
-	mid = len(ary)/2
-	if len(ary) > 1:  #분할
-		MrgSort(ary[:mid])  #좌분할
-		MrgSort(ary[mid:])  #우분할
-		#Mrg(a, b)
-
 def Mrg(trg1, trg2):  #합병 모듈
 	res = []
-	idx_trg1 = 0
-	idx_trg2 = 0
-	while (idx_trg1 <= len(trg1) - 1) and (idx_trg2 <= len(trg2) - 1):
-		if trg1[idx_trg1] < trg2[idx_trg2]:
-			res.append(trg1[idx_trg1])
-			idx_trg1 += 1
-		else:
-			res.append(trg2[idx_trg2])
-			idx_trg2 += 1
-	if (idx_trg1 > len(trg1) - 1) and (idx_trg2 < len(trg2) - 1):  #trg1은 전부 추가됨 & trg2는 남은 게 있음.
-		for i in range(idx_trg2, len(trg2)):
-			res.append(trg2[i])
-	elif (idx_trg1 < len(trg1) - 1) and (idx_trg2 > len(trg2) - 1):  #trg1은 남은 게 있음 & trg2는 전부 추가됨.
-		for i in range(idx_trg1, len(trg1)):
+	a = len(trg1)
+	b = len(trg2)
+	i = j = 0
+	while (i < a) and (j < b):
+		if trg1[i] < trg2[j]:
 			res.append(trg1[i])
+			i += 1
+		else:
+			res.append(trg2[j])
+			j += 1
+	res += trg1[i:]
+	res += trg2[j:]
 	return res
 
-"""
-def Compare(ary1, idx_ary1, ary2, idx_ary2):
-	res = []
-	pos = 0
-	if ary1[idx_ary1] < ary2[idx_ary2]:
-		res.append(ary1[idx_ary1])
-		idx_ary1 += 1
-		pos += 1
-	else:
-		res.append(ary2[idx_ary2])
-		idx_ary2 += 1
-		pos += 1
-	return (res, pos)
+def MrgSort(ary):
+	if len(ary) <= 1:  #분할
+		print(ary)
+		return ary
 
-Compare(trg1, idx_trg1, trg2, idx_trg2)
-"""
+	mid = int(len(ary)/2)
+	ary_l = MrgSort(ary[:mid])  #좌분할
+	ary_r = MrgSort(ary[mid:])  #우분할
+	print(ary_l, ary_r)
+	return Mrg(ary_l, ary_r)
+
 if __name__ == '__main__':
-	#MrgSort([47, 12, 32, 15, 55, 81, 97, 17])
-	print(Mrg([47, 12, 32, 15], [55, 81, 97, 17]))
-
+	print(MrgSort([47, 12, 32, 15, 55, 81, 97, 17]))
