697f518b7e657f7d7b3ec636a113e14777536c91
Plentir <yjy990810@naver.com>
2019-04-20 14:10:50 +0900

    됴깐네

--- 알고리즘/과제/4. Merge Sort_20182827 윤주영.py
+++ 알고리즘/과제/4. Merge Sort_20182827 윤주영.py
@@ -1,17 +1,46 @@
 # 합병 정렬
-def MrgSort(ary_input, low, high):
-	mid = (low + high)/2
-	if len(ary_input) >= 2 and low == 1:
-		return MrgSort(ary_input, low, mid)
-	elif len(ary_input) >= 2 and high == (len(ary_input) - 1):
-		return MrgSort(ary_input, mid, high)
-	
+def MrgSort(ary):
+	mid = len(ary)/2
+	if len(ary) > 1:  #분할
+		a = MrgSort(ary[:mid])  #좌분할
+		b = MrgSort(ary[mid:])  #우분할
+		Mrg(a, b)
+
+def Mrg(trg1, trg2):
+	res = []
+	pos = 0
+	idx_trg1 = 0
+	idx_trg2 = 0
+	while (idx_trg1 <= len(trg1) - 1) and (idx_trg2 <= len(trg2) - 1):
+		if trg1[idx_trg1] < trg2[idx_trg2]:
+			res.append(trg1[idx_trg1])
+			idx_trg1 += 1
+			pos += 1
+		else:
+			res.append(trg2[idx_trg2])
+			idx_trg2 += 1
+			pos += 1
+	if 
+		
+
+	return res
+"""
+def Compare(ary1, idx_ary1, ary2, idx_ary2):
+	res = []
+	pos = 0
+	if ary1[idx_ary1] < ary2[idx_ary2]:
+		res.append(ary1[idx_ary1])
+		idx_ary1 += 1
+		pos += 1
 	else:
-		ary = ary_input[low - 1 : 
-		mid = (low + high)/2
-	
-	
-	
+		res.append(ary2[idx_ary2])
+		idx_ary2 += 1
+		pos += 1
+	return (res, pos)
+
+Compare(trg1, idx_trg1, trg2, idx_trg2)
+"""
 if __name__ == '__main__':
-	MrgSort([47, 12, 32, 15, 55, 81, 97, 17], 1, 8)
+	#MrgSort([47, 12, 32, 15, 55, 81, 97, 17])
+	print(Mrg([47, 12, 32, 15], [55, 81, 97, 17]))
 
