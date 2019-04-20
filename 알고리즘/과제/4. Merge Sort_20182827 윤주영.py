75fd7725718f414929276658ef41c28778429277
Plentir <yjy990810@naver.com>
2019-04-20 18:20:14 +0900

    합병 모듈 완료

--- 알고리즘/과제/4. Merge Sort_20182827 윤주영.py
+++ 알고리즘/과제/4. Merge Sort_20182827 윤주영.py
@@ -2,28 +2,29 @@
 def MrgSort(ary):
 	mid = len(ary)/2
 	if len(ary) > 1:  #분할
-		a = MrgSort(ary[:mid])  #좌분할
-		b = MrgSort(ary[mid:])  #우분할
-		Mrg(a, b)
+		MrgSort(ary[:mid])  #좌분할
+		MrgSort(ary[mid:])  #우분할
+		#Mrg(a, b)
 
-def Mrg(trg1, trg2):
+def Mrg(trg1, trg2):  #합병 모듈
 	res = []
-	pos = 0
 	idx_trg1 = 0
 	idx_trg2 = 0
 	while (idx_trg1 <= len(trg1) - 1) and (idx_trg2 <= len(trg2) - 1):
 		if trg1[idx_trg1] < trg2[idx_trg2]:
 			res.append(trg1[idx_trg1])
 			idx_trg1 += 1
-			pos += 1
 		else:
 			res.append(trg2[idx_trg2])
 			idx_trg2 += 1
-			pos += 1
-	if 
-		
-
+	if (idx_trg1 > len(trg1) - 1) and (idx_trg2 < len(trg2) - 1):  #trg1은 전부 추가됨 & trg2는 남은 게 있음.
+		for i in range(idx_trg2, len(trg2)):
+			res.append(trg2[i])
+	elif (idx_trg1 < len(trg1) - 1) and (idx_trg2 > len(trg2) - 1):  #trg1은 남은 게 있음 & trg2는 전부 추가됨.
+		for i in range(idx_trg1, len(trg1)):
+			res.append(trg1[i])
 	return res
+
 """
 def Compare(ary1, idx_ary1, ary2, idx_ary2):
 	res = []
