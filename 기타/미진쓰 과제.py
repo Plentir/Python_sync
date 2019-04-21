e068a7f98a3379cbfcf0ccf9ee9d9b61c7c4da45
Plentir <yjy990810@naver.com>
2019-04-21 18:53:18 +0900

    모듈화

--- 기타/미진쓰 과제.py
+++ 기타/미진쓰 과제.py
@@ -1,31 +1,37 @@
-fom = input("""계산할 수식을 입력하세요.
+def Calc(formular):
+    if "^" in formular:
+        idx = formular.index("^")
+        formular.insert(idx - 1, float(formular[idx - 1]) ** float(formular[idx + 1]))
+
+    elif "*" in formular:
+        idx = formular.index("*")
+        formular.insert(idx - 1, float(formular[idx - 1]) * float(formular[idx + 1]))
+
+    elif "/" in formular:
+        idx = formular.index("/")
+        formular.insert(idx - 1, float(formular[idx - 1]) / float(formular[idx + 1]))
+
+    elif "+" in formular:
+        idx = formular.index("+")
+        formular.insert(idx - 1, float(formular[idx - 1]) + float(formular[idx + 1]))
+
+    elif "-" in formular:
+        idx = formular.index("-")
+        formular.insert(idx - 1, float(formular[idx - 1]) - float(formular[idx + 1]))
+
+    del formular[idx : idx + 3]
+    return formular
+
+def RunCalculator():
+    fom = input("""계산할 수식을 입력하세요.
 예) 1 + 3 * 3 ... (주의: 수와 연산 기호(+, -, *, ^, /)는 띄어쓰기로 구분합니다.)
 입력: """).split()
 
-while len(fom) >= 3:
-    if "^" in fom:
-        idx = fom.index("^")
-        fom.insert(idx - 1, float(fom[idx - 1]) ** float(fom[idx + 1]))
-        del fom[idx : idx + 3]
+    while len(fom) >= 3:
+        Calc(fom)
 
-    elif "*" in fom:
-        idx = fom.index("*")
-        fom.insert(idx - 1, float(fom[idx - 1]) * float(fom[idx + 1]))
-        del fom[idx : idx + 3]
+    print("계산 결과: %s" %fom[0])
+    return fom[0]
 
-    elif "/" in fom:
-        idx = fom.index("/")
-        fom.insert(idx - 1, float(fom[idx - 1]) / float(fom[idx + 1]))
-        del fom[idx : idx + 3]
-
-    elif "+" in fom:
-        idx = fom.index("+")
-        fom.insert(idx - 1, float(fom[idx - 1]) + float(fom[idx + 1]))
-        del fom[idx : idx + 3]
-
-    elif "-" in fom:
-        idx = fom.index("-")
-        fom.insert(idx - 1, float(fom[idx - 1]) - float(fom[idx + 1]))
-        del fom[idx : idx + 3]
-
-print("계산 결과: %s" %fom[0])
+if __name__ == '__main__':
+    RunCalculator()
