6e04e436a7e8f6d06b9c4ee71a01a638825c2cdd
Plentir <yjy990810@naver.com>
2019-05-05 04:59:14 +0900

    문제 수정

--- 기타/Get Subdirectories Name.py
+++ 기타/Get Subdirectories Name.py
@@ -1,7 +1,5 @@
 import os
-"""
-폴더 트리에서 2단계 이상 들어가면 해당 폴더와 그 하위 파일의 들여쓰기가 잘못됨.
-"""
+
 def GetNameAllSubdir(dirs):  # 모든 하위 디렉터리의 목록을 텍스트 파일로 저장.
     dir_desktop = os.environ["USERPROFILE"]
     f = open("%s\\Desktop\\Name Subdirectories_All.txt" %dir_desktop, "w")
@@ -14,17 +12,17 @@ def GetNameAllSubdir(dirs):  # 모든 하위 디렉터리의 목록을 텍스트
             continue
 
         elif len(name_subdir) >= 3:  # 하위 폴더 반환
-            f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)) - 1)), name_subdir[-1]))
+            f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)[-1].split("\\")) - 2)), name_subdir[-1]))
             
             for j in i[-1]:  # 폴더 내 파일 반환
-                f.write("%s└%s\n" %((" " * 3 * len(i[0].split(dirs))), j))
+                f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)[-1].split("\\")) - 1)), j))
 
         else:  # 최상위 폴더 반환
             f.write("%s\n" %(name_subdir[-1]))
 
             if len(i[-1]) != 0:
                 for j in i[-1]:  # 폴더 내 파일 반환
-                    f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)) - 1)), j))
+                    f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)[-1].split("\\")) - 1)), j))
             
             else:
                 continue
