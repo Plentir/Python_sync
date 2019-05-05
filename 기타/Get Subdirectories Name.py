715674c33a3f3d9c1e42c7493713343f8f5061e1
Plentir <yjy990810@naver.com>
2019-05-05 23:31:12 +0900

    조금 더 깔끔하게?

--- 기타/Get Subdirectories Name.py
+++ 기타/Get Subdirectories Name.py
@@ -1,8 +1,8 @@
 import os
 
 def GetNameAllSubdir(dirs):  # 모든 하위 디렉터리의 목록을 텍스트 파일로 저장.
-    dir_desktop = os.environ["USERPROFILE"]
-    f = open("%s\\Desktop\\Name Subdirectories_All.txt" %dir_desktop, "w")
+    dir_desktop = os.environ["USERPROFILE"] + "\\Desktop"
+    f = open("%s\\Subdirectories' Name_All.txt" %dir_desktop, "w")
 
     for i in os.walk(dirs):
         name_subdir = i[0].split(dirs)[-1].split("\\")
@@ -12,17 +12,17 @@ def GetNameAllSubdir(dirs):  # 모든 하위 디렉터리의 목록을 텍스트
             continue
 
         elif len(name_subdir) >= 3:  # 하위 폴더 반환
-            f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)[-1].split("\\")) - 2)), name_subdir[-1]))
+            f.write("%s└%s\n" %((" " * 3 * (len(name_subdir) - 2)), name_subdir[-1]))
             
             for j in i[-1]:  # 폴더 내 파일 반환
-                f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)[-1].split("\\")) - 1)), j))
+                f.write("%s└%s\n" %((" " * 3 * (len(name_subdir) - 1)), j))
 
         else:  # 최상위 폴더 반환
             f.write("%s\n" %(name_subdir[-1]))
 
             if len(i[-1]) != 0:
                 for j in i[-1]:  # 폴더 내 파일 반환
-                    f.write("%s└%s\n" %((" " * 3 * (len(i[0].split(dirs)[-1].split("\\")) - 1)), j))
+                    f.write("%s└%s\n" %((" " * 3 * (len(name_subdir) - 1)), j))
             
             else:
                 continue
@@ -30,12 +30,12 @@ def GetNameAllSubdir(dirs):  # 모든 하위 디렉터리의 목록을 텍스트
         f.write("\n")  # 디렉터리 변동 후 줄 바꿈
 
     f.close()
-    print("Done.")
+    print("The report file has been created in %s." %dir_desktop)
 
     return 1
 
 
 if __name__ == "__main__":
-    trg = input("파일 목록을 작성할 디렉터리를 입력하세요.\n입력: ")
-    # D:\\Media\\영상\\Movies\\해외 영화
+    # trg = input("파일 목록을 작성할 디렉터리를 입력하세요.\n입력: ")
+    trg = "D:\\Media\\영상\\Movies\\해외 영화"
     GetNameAllSubdir(trg)
