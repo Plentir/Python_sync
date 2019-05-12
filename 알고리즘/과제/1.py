72be8e635b578419d3a4554af20f67f172fdabb4
Plentir <yjy990810@naver.com>
2019-05-12 11:21:38 +0900

    오류 수정

--- 알고리즘/과제/1.py
+++ 알고리즘/과제/1.py
@@ -11,15 +11,19 @@ def Kruskal(array_weights, array_sections):
         for i in range(len(array_weights)):  # 가중치가 가장 작은 연결을 검색. i는 연결이 시작되는 노드의 번호(index).
 
             for j in range(len(array_weights[i])):  # j는 i와 연결될 노드의 번호(index).
-
                 if array_weights[i][j] < _min and array_weights[i][j] != -1:
                     _min = array_weights[i][j]
                     _from, _to = i, j
 
         if array_sections[_from] != array_sections[_to]:  # 두 노드를 연결했을 때 loop가 발생하지 않으면 두 노드를 연결함.
 
-            for k in range(array_sections.count(_to + 1)):
-                array_sections[array_sections.index(_to + 1)] = array_sections[_from]  # 섹션 수정.
+            if array_sections.count(_to + 1) == 0:  # 섹션 통합
+                for k in range(array_sections.count(_from + 1)):
+                    array_sections[array_sections.index(_from + 1)] = array_sections[_to]
+
+            else:
+                for k in range(array_sections.count(_to + 1)):  # 섹션 통합
+                    array_sections[array_sections.index(_to + 1)] = array_sections[_from]
 
             array_weights[_from][_to] = array_weights[_to][_from] = -1
 
@@ -42,12 +46,12 @@ def Kruskal(array_weights, array_sections):
 if __name__ == "__main__":
     
     # (6, 9)
-    weights1 = [[-1, 6, 7, 999, 10, 9],
-               [6, -1, 999, 999, 999, 999], 
-               [7, 8, -1, 4, 5, 999], 
-               [999, 999, 4, -1, 3, 999], 
-               [10, 999, 5, 3, -1, 11], 
-               [9, 999, 999, 999, 11]]
+    weights1 = [[-1, 6, 7, 999, 10, 9],  # Node 1
+               [6, -1, 999, 999, 999, 999],  # Node 2
+               [7, 8, -1, 4, 5, 999],  # Node 3
+               [999, 999, 4, -1, 3, 999],  # Node 4
+               [10, 999, 5, 3, -1, 11],  # Node 5
+               [9, 999, 999, 999, 11]]  # Node 6
 
     sections1 = [1, 2, 3, 4, 5, 6]
 
@@ -63,4 +67,17 @@ if __name__ == "__main__":
 
     sections2 = [1, 2, 3, 4, 5, 6, 7, 8]
 
+    # (5, 8)
+    weights3 = [[-1, 4, 4, 3, 6], # Node 1
+                [4, -1, 3, 999, 7], # Node 2
+                [4, 3, -1, 2, 999], # Node 3
+                [3, 999, 2, -1, 5], # Node 4
+                [6, 7, 999, 5, -1]] # Node 5
+
+    sections3 = [1, 2, 3, 4, 5]
+
+    # Kruskal(weights1, sections1)
+    # print()
     Kruskal(weights2, sections2)
+    # print()
+    # Kruskal(weights3, sections3)
