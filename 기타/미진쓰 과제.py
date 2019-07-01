47980a5d4991dfaae2db309008a2a84b7f03d51b
Plentir <yjy990810@naver.com>
2019-07-01 15:42:19 +0900

    test

--- 기타/미진쓰 과제.py
+++ 기타/미진쓰 과제.py
@@ -22,6 +22,14 @@ def Calc(formular):
     del formular[idx : idx + 3]
     return formular
 
+
+def operators(f, opr):
+    index = f.index(opr)
+    f.insert(index - 1, float(f[index - 1]))
+
+    return f
+
+
 def RunCalculator():
     fom = input("""계산할 수식을 입력하세요.
 예) 1 + 3 * 3 ... (주의: 수와 연산 기호(+, -, *, ^, /)는 띄어쓰기로 구분합니다.)
@@ -32,6 +40,7 @@ def RunCalculator():
 
     print("계산 결과: %s" %fom[0])
     return fom[0]
+
 
 if __name__ == '__main__':
     RunCalculator()
