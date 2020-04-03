02f77601e70f3233b7449b65e5b00127c5c2c0b5
Plentir <plentir@naver.com>
2020-04-03 22:21:59 +0900

    디렉터리 구분 방식 변경

--- 기타/Get Subdirectories Name.py
+++ 기타/Get Subdirectories Name.py
@@ -2,35 +2,36 @@ import os
 
 def GetNameAllSubdir(dirs):  # 모든 하위 디렉터리의 목록을 텍스트 파일로 저장.
     dir_desktop = os.environ["USERPROFILE"] + "\\Desktop"
-    f = open("%s\\Subdirectories' Name_All.txt" %dir_desktop, "w", encoding="utf-8")
+    dir_trg = dirs.split(sep="\\\\")[-1]
+    f = open("%s\\File list_%s.txt" %(dir_desktop, dir_trg), "w", encoding="utf-8")
 
     for i in os.walk(dirs):
         name_subdir = i[0].split(dirs)[-1].split("\\")
         
         if i[0] is dirs:
-            f.write("The list of a files in " + dirs + "\n\n")
+            f.write("The list of files in " + dirs + "\n" + 60*"=" + "\n\n")
             continue
 
         elif len(name_subdir) >= 3:  # 하위 폴더 반환
-            f.write("%s└%s\n" %((" " * 3 * (len(name_subdir) - 2)), name_subdir[-1]))
+            f.write("%s└ [dir] %s\n" %((" " * 3 * (len(name_subdir) - 2)), name_subdir[-1]))
             
             for j in i[-1]:  # 폴더 내 파일 반환
-                f.write("%s└%s\n" %((" " * 3 * (len(name_subdir) - 1)), j))
+                f.write("%s└ %s\n" %((" " * 3 * (len(name_subdir) - 1)), j))
 
         else:  # 최상위 폴더 반환
-            f.write("%s\n" %(name_subdir[-1]))
+            f.write("[dir] %s\n" %(name_subdir[-1]))
 
             if len(i[-1]) != 0:
                 for j in i[-1]:  # 폴더 내 파일 반환
-                    f.write("%s└%s\n" %((" " * 3 * (len(name_subdir) - 1)), j))
+                    f.write("%s└ %s\n" %((" " * 3 * (len(name_subdir) - 1)), j))
             
             else:
                 continue
         
-        f.write("\n")  # 디렉터리 변경 후 줄 바꿈
+        #f.write("\n")  # 디렉터리 변경 후 줄 바꿈
 
     f.close()
-    print("The report file has been created in %s." %dir_desktop)
+    print("The report file is created in %s." %dir_desktop)
 
     return 1
 
