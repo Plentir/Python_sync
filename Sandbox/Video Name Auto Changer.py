fe623724ae9bf534461d2d5ee264d6cd30659eee
Plentir <plentir@naver.com>
2020-05-05 13:31:36 +0900

    초기 완성

--- Sandbox/Video Name Auto Changer.py
+++ Sandbox/Video Name Auto Changer.py
@@ -7,18 +7,11 @@ class AutoChanger():
 
         self.__file_names = os.listdir(self.__dir)
         self.__file_formats = [f[-3:] for f in self.__file_names]
+        
+        return
 
 
-    def setRules(self, title="none", year="none", sep=" "):
-        if title == "none":
-            self.__title = self.__dir.split("\\")[-1]
-
-        else:
-            self.__title = title
-
-        self.__year = year
-        self.__sep = sep
-
+    def getRules(self):
         if self.__sep == " ":
             print("Naming rules updated!\n >Directory: %s\n >Title: %s\n >Year: %s\n >Separator: %s" \
                 %(self.__dir, self.__title, self.__year, "Blank"))
@@ -31,6 +24,23 @@ class AutoChanger():
             print("Naming rules updated!\n >Directory: %s\n >Title: %s\n >Year: %s\n >Separator: %s" \
                 %(self.__dir, self.__title, self.__year, self.__sep))
 
+        return
+
+
+    def setRules(self, title="none", year="none", sep=" "):
+        if title == "none":
+            self.__title = self.__dir.split("\\")[-1]
+
+        else:
+            self.__title = title
+
+        self.__year = year
+        self.__sep = sep
+
+        self.getRules()
+
+        return
+
 
     def getFiles(self):
         file_no = 1
@@ -39,6 +49,8 @@ class AutoChanger():
             print("File_%s: %s" %(file_no, l))
             file_no += 1
     
+        return
+
 
     def getFlieForms(self):
         file_no = 1
@@ -46,9 +58,34 @@ class AutoChanger():
         for l in self.__file_formats:
             print("File_%s: %s" %(file_no, l))
             file_no += 1
+        
+        return
+        
+    
+    def changeName(self):
+        self.getRules()
+        flag = input("계속 진행할까요? [y, n]\n입력: ")
+
+        if flag in ("n", "N", "no", "No", "NO"):
+            print("취소됨.")
+            return
+
+        elif flag in ("y", "Y", "yes", "Yes", "YES"):
+            os.chdir(self.__dir)
+            file_no = 0
+
+            for name in self.__file_names:
+                os.rename(name, "%s - %02d.%s" %(self.__title, (file_no + 1), self.__file_formats[file_no]))
+                file_no += 1
+
+        print("완료됨.")
+
+        return
+            
 
 if __name__ == "__main__":
-    test = AutoChanger("D:\\Media\\영상\\Movies\\애니메이션\\Pop Team Epic")
+    test = AutoChanger("D:\\Media\\영상\\Movies\\애니메이션\\카게구루이")
     
     test.setRules()
     test.getFiles()
+    test.changeName()
